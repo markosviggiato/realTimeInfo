a35c8eefd21fee8396b832d787fb6dac4778a221
markosviggiato <markosviggiato@gmail.com>
2018-08-24 15:42:49 -0300

    just some minor adjustments in the scripts to improve readability

--- CNNCrawler.R
+++ CNNCrawler.R
@@ -1,24 +1,29 @@
-### This code makes use of CNN News API to collect data
 ### Author : Markos Viggiato
 ### Date : August 23rd, 2018
+### This code makes use of CNN News API to collect data
 ### Powered by CNN News API (newsapi.org)
 
 #################
 
-# package to access CNN News API and to make the http requests
+# useful packages
 library(newsAPI)
 library("jsonlite")
 
+# main function to search for articles
 CNNCrawler <- function(){
+      
+      # read access keys
+      secret <- read.table("secret.file", stringsAsFactors = FALSE)
+      apikey <- secret[[1]][[5]]
     
-    # read access keys
-    secret <- read.table("secret.file", stringsAsFactors = FALSE)
-    apikey <- secret[[1]][[5]]
-    
-    # HTTP request to teh REST API
-    repoInfo <- fromJSON(paste0("https://newsapi.org/v2/top-headlines?sources=cnn&q=Trump&apiKey=", apikey))
-    d <- repoInfo$articles
-    d <- d[d$source$name=="CNN",]
-    d
-    
+      # HTTP request to the REST API - articles containing the word 'Trump'
+      repoInfo <- fromJSON(paste0("https://newsapi.org/v1/articles?source=cnn&q=trump&apiKey=",apikey))
+      dataCNN <- repoInfo$articles
+
+      #select the latest 25 articles
+      if(length(dataCNN$title) > 25)
+            dataCNN <- dataCNN[1:25,]
+      
+      # return the data frame with article's information
+      dataCNN
 }
--- twitterCrawler.R
+++ twitterCrawler.R
@@ -1,29 +1,31 @@
-### This code makes use of Twitter API to collect data
 ### Author : Markos Viggiato
 ### Date : August 23rd, 2018
+### This code makes use of Twitter API to collect data
 
 #################
 
-# package to access Twitter API
+# useful packages
 library("twitteR")
 
-
+# main function to collect tweets from Donald Trump
 twitterCrawler <- function(){
     
-    # read access keys
-    secret <- read.table("secret.file", stringsAsFactors = FALSE)
-    consumer_key <- secret[[1]][[1]]
-    consumer_secret <- secret[[1]][[2]]
-    access_token <- secret[[1]][[3]]
-    access_secret <- secret[[1]][[4]]
+      # read access keys
+      secret <- read.table("secret.file", stringsAsFactors = FALSE)
+      consumer_key <- secret[[1]][[1]]
+      consumer_secret <- secret[[1]][[2]]
+      access_token <- secret[[1]][[3]]
+      access_secret <- secret[[1]][[4]]
     
-    # setup authentication information
-    setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
+      # setup authentication information
+      setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
     
-    # get the 25 most recent tweets from the user specified n the first argument of 'userTimeline'
-    tw <- userTimeline("realDonaldTrump", n = 26)
+      # get the 25 most recent tweets from the user specified n the first argument of 'userTimeline'
+      tw <- userTimeline("realDonaldTrump", n = 27)
     
-    # convert to dataframe
-    d = twitteR::twListToDF(tw)
-    d
+      # convert to dataframe
+      dataTwitter = twitteR::twListToDF(tw)
+    
+      # return the data frame with the 25 latest tweets
+      dataTwitter
 }
